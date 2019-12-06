f5e0263d8c341a89c111c8bd59dbba2a350c16dd
Evelina <evelinawahlstrom18@gmail.com>
2019-12-06 09:09:49 +0400

    format-fix

--- users/router.js
+++ users/router.js
@@ -6,24 +6,24 @@ const router = new Router();
 
 router.post("/register", (req, res, next) => {
     console.log("A req on /register")
-    const fullName= req.body.fullName
+    const fullName = req.body.fullName
     const email = req.body.email
     const password = req.body.password
     if (!fullName || !email || !password) {
         res.status(409).send({
             message: "Please supply a fullname and a valid email + password"
-        })   
-    }else{
+        })
+    } else {
         User.create({
             fullName: fullName,
             email: email,
             password: bcrypt.hashSync(req.body.password, 10)
         })
-        .then(user => {
-            res.status(201)
-            res.send({status:"OK"})
-        .catch(console.error)
-        } )
+            .then(user => {
+                res.status(201)
+                res.send({ status: "OK" })
+            .catch(console.error)
+            })
     }
-  })
-  module.exports = router;
+})
+module.exports = router;
